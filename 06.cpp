6c39f5ac6cd9024cf31aaeec678ba16d950963d0
rancho <dyzhuo@zju.edu.cn>
2019-04-27 00:02:24 +0800

    leetcode 06 and CMakeLists.txt

---
+++ 06.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+using namespace std;
+/*
+ Example 1:
+
+Input: s = "PAYPALISHIRING", numRows = 3
+Output: "PAHNAPLSIIGYIR"
+Example 2:
+
+Input: s = "PAYPALISHIRING", numRows = 4
+Output: "PINALSIGYAHRPI"
+        PINALSIGYAHRPI
+Explanation:
+
+P     I    N
+A   L S  I G
+Y A   H R
+P     I
+ */
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        string out;
+        if(s.size()<3 || numRows==1){
+            return s;
+        }
+        queue<int> qe[numRows];
+        int now=-1;
+        int inc=1;
+        for(int i=0;i<s.size();i++){
+            now+=inc;
+            if(now<0){
+                inc=1;
+                now+=2;
+            }else if(now>=numRows){
+                inc=-1;
+                now-=2;
+            }
+            qe[now].push(s[i]);
+        }
+        for(int i=0;i<numRows;i++){
+            //        printf("%d\n",qe[i].size());
+            while(!qe[i].empty()){
+                out+=qe[i].front();
+                qe[i].pop();
+            }
+        }
+        return out;
+    }
+};
+
+int main(){
+    string s;
+    int numRows;
+    cin>>s>>numRows;
+    queue<int> qe[numRows];
+    int now=-1;
+    int inc=1;
+    for(int i=0;i<s.size();i++){
+         now+=inc;
+         if(now<0){
+             inc=1;
+             now+=2;
+         }else if(now>=numRows){
+             inc=-1;
+             now-=2;
+         }
+         qe[i].push(s[i]);
+    }
+    for(int i=0;i<numRows;i++){
+//        printf("%d\n",qe[i].size());
+        while(!qe[i].empty()){
+            printf("%c",qe[i].front());
+            qe[i].pop();
+        }
+    }
+    return 0;
+}
+
