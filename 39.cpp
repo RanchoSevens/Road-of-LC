ac7622ced1d8eca1abfc7c9971b7acb95ce08973
RanchoSevens <rcdy35@gmail.com>
2019-08-20 16:05:37 +0800

    Update 39.cpp

--- 39.cpp
+++ 39.cpp
@@ -3,22 +3,29 @@ public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         const int len = candidates.size();
         vector<vector<int>> result;
-        vector<int> count(len,0);
-        int sum;
+        vector<vector<int>> nodr;
+        vector<int> count;
+        int sum=0;
         for(int i=0;i<len;++i){
-            if(candidates[i]<=target){
-                sum = candidates[i];
-                count[i]++;
-                vector<int> ans = dfs(candidates,count,target,sum);
-                if(!ans.empty()){
-                    result.push_back(ans);
-                }    
-            }
-            
+            int k=candidates[i];
+            count.push_back(k);
+            dfs(candidates,result,count,target,sum+k,i,len);  
+            count.pop_back();
         }
-        return ans;
+        return result;
     }
-    vector<int> dfs(vector<int>& candidates,vector<int>& count,int target,int sum){
-        
+    void dfs(vector<int>& candidates,vector<vector<int>> &result,vector<int> &count,int target,int sum,int i,int len){
+        if(sum==target){
+            result.push_back(count);
+            return;
+        }
+        if(sum>target) return;
+        for(int j=i;j<len;++j){
+            int k=candidates[j];
+            count.push_back(k);
+            dfs(candidates,result,count,target,sum+k,j,len);
+            count.pop_back();
+        }
+        return;
     }
 };
