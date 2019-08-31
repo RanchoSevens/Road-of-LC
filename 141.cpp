8026b3729d897a75a12807ec92eace471f75715a
RanchoSevens <rcdy35@gmail.com>
2019-08-31 12:27:15 +0800

    Update 141.cpp

--- 141.cpp
+++ 141.cpp
@@ -21,3 +21,19 @@ public:
         return result;
     }
 };
+
+
+//solution不改变链表本身的解法
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode walker = head;
+        ListNode runner = head;
+        while(runner!=null && runner.next!=null) {
+            walker = walker.next;
+            runner = runner.next.next;
+            if(walker==runner) return true;
+        }
+        return false;
+    }
+}
+
