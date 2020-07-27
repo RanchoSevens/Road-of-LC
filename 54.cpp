614712d00a1dfb40a4b158afe07399aea7ffbb7e
RanchoSevens <dyzhuo@zju.edu.cn>
2020-07-27 22:29:04 +0800

    Update 54.cpp

--- 54.cpp
+++ 54.cpp
@@ -33,3 +33,41 @@ public:
 1.按圈循环处理
 2.在只剩一行或只剩一列时，在输出行或输出列后跳出
 */
+
+/* Others
+
+Visualization
+Here's how the matrix changes by always extracting the first row and rotating the remaining matrix counter-clockwise:
+
+    |1 2 3|      |6 9|      |8 7|      |4|  =>  |5|  =>  ||
+    |4 5 6|  =>  |5 8|  =>  |5 4|  =>  |5|
+    |7 8 9|      |4 7|
+Now look at the first rows we extracted:
+
+    |1 2 3|      |6 9|      |8 7|      |4|      |5|
+Those concatenated are the desired result.
+
+Another visualization
+  spiral_order([[1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9]])
+
+= [1, 2, 3] + spiral_order([[6, 9],
+                            [5, 8],
+                            [4, 7]])
+
+= [1, 2, 3] + [6, 9] + spiral_order([[8, 7],
+                                     [5, 4]])
+
+= [1, 2, 3] + [6, 9] + [8, 7] + spiral_order([[4],
+                                              [5]])
+
+= [1, 2, 3] + [6, 9] + [8, 7] + [4] + spiral_order([[5]])
+
+= [1, 2, 3] + [6, 9] + [8, 7] + [4] + [5] + spiral_order([])
+
+= [1, 2, 3] + [6, 9] + [8, 7] + [4] + [5] + []
+
+= [1, 2, 3, 6, 9, 8, 7, 4, 5]
+
+*/
