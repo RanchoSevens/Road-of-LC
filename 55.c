6e2686c95005070245894e83bee1fa4b5e41dd0d
RanchoSevens <dyzhuo@zju.edu.cn>
2020-08-05 23:09:56 +0800

    Update 55.c

--- 55.c
+++ 55.c
@@ -3,10 +3,11 @@ bool canJump(int* nums, int numsSize){
     for(i=0,reach=i;i<numsSize&&i<=reach;i++){
         int k=reach>(i+nums[i])?reach:i+nums[i];
         if(reach<k) reach = k;
+        if(reach>=numsSize-1) return true;
     }
-    return i==numsSize;
+    return false;
 }
 /*
 1.reach来控制能到达的最远位置(下标)
-2.i由numsSize和reach两个共同控制，到达numsSize说明最后一个位置也可以走过去
+2.当reach到达最后一个下标，返回true，否则false
 */
