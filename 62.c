b3457a2f986aa3cbc677e4b5227f8c48838c510c
RanchoSevens <dyzhuo@zju.edu.cn>
2020-08-09 18:24:36 +0800

    Update 62.c

--- 62.c
+++ 62.c
@@ -2,6 +2,8 @@
 1.熟悉递归调用、为递归调用
 2.在使用递归之前函数需先声明
 3.排列组合相关知识
+    1.首先从组合的角度，m个1和n个0，不重复的组合有 C n (m+n) 种
+    2.其次从排列的角度，(m+n)!的全排列，要减去多余的同模式的排列，同模式有(m!)*(n!)个，因此就是 (m+n)!/((m!)*(n!))
 */
 long int comb(int m,int n,long int total);
 long int frac(int n,long int total);
