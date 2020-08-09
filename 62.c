4268e0b3564c32ecb3ea47f429235f2028b26ba4
RanchoSevens <dyzhuo@zju.edu.cn>
2020-08-09 16:47:00 +0800

    Create 62.c

---
+++ 62.c
@@ -0,0 +1,26 @@
+long int comb(int m,int n,long int total);
+long int frac(int n,long int total);
+
+int uniquePaths(int m, int n){
+    if(m==1||n==1) return 1;
+    if(m<n){
+        int k=m;
+        m=n;
+        n=k;
+    }
+    return comb(m-1,n-1,1)/frac(n-1,1);
+}
+
+long int comb(int m,int n,long int total){
+    if(n==0) return total;
+    return comb(m,n-1,total*(m+n));
+}
+long int frac(int n,long int total){
+    if(n==1) return total;
+    return frac(n-1,total*n);
+}
+/*
+1.熟悉递归调用、为递归调用
+2.在使用递归之前函数需先声明
+3.排列组合相关知识
+*/
