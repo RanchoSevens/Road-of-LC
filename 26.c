86beb7e3452636420e4b51c17eb16591525184f2
RanchoSevens <dyzhuo@zju.edu.cn>
2020-11-10 11:27:27 +0800

    Update 26.c

--- 26.c
+++ 26.c
@@ -1,14 +1,12 @@
 /*
-  1. 当仅当相邻数字不一致时更新id
+  1. 当仅当相邻数字不一致时更新i
 */
 
-class Solution {
-    public:
-    int removeDuplicates(int A[], int n) {
-        if(n < 2) return n;
-        int id = 1;
-        for(int i = 1; i < n; ++i) 
-            if(A[i] != A[i-1]) A[id++] = A[i];
-        return id;
-    }
-};
+
+int removeDuplicates(int* nums, int numsSize){
+    int i,now;
+    for(i=1,now=i;now<numsSize;++now)
+        if(nums[i-1] < nums[now])
+            nums[i++] = nums[now];
+    return i;
+}
