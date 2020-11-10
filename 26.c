5283b4b3fec12b0f14e0a2966b723b923febc96f
RanchoSevens <dyzhuo@zju.edu.cn>
2020-11-10 11:29:38 +0800

    Update 26.c

--- 26.c
+++ 26.c
@@ -2,8 +2,8 @@
   1. 当仅当相邻数字不一致时更新i
 */
 
-
 int removeDuplicates(int* nums, int numsSize){
+    if(numsSize<1) return 0;
     int i,now;
     for(i=1,now=i;now<numsSize;++now)
         if(nums[i-1] < nums[now])
