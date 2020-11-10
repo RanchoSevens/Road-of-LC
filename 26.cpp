4c16f416833035cc1149ff2a757832e29e8b4c38
RanchoSevens <dyzhuo@zju.edu.cn>
2020-11-10 11:27:52 +0800

    Update 26.cpp

--- 26.cpp
+++ 26.cpp
@@ -12,3 +12,21 @@ public:
         return nums.size()==0?k:(k+1);
     }
 };
+
+
+/*
+  1. 当仅当相邻数字不一致时更新id
+*/
+
+
+
+class Solution {
+    public:
+    int removeDuplicates(int A[], int n) {
+        if(n < 2) return n;
+        int id = 1;
+        for(int i = 1; i < n; ++i) 
+            if(A[i] != A[i-1]) A[id++] = A[i];
+        return id;
+    }
+};
